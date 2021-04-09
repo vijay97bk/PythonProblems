b69c2d54a6b0a14d21a1a6610552a51d680a1184
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-04-09 11:36:00 +0530

    [vijay]AddStopWatchProblem

---
+++ Logical Programs/StopWatch.py
@@ -0,0 +1,25 @@
+'''
+date = '08/04/2021'
+modified_date = '08/04/2021'
+author = 'Vijay Kshirasagar'
+description = 'Write a Stopwatch Program for measuring the time that elapses between the start and end clicks'
+'''
+
+import os    
+import time    
+second = 0    
+minute = 0    
+hours = 0    
+while(True):    
+    print("Simple Stopwatch(in Python) Created By Sourabh Somani...")     
+    print('-------------')    
+    print(' %d : %d : %d '%(hours,minute,second))    
+    print('-------------')    
+    time.sleep(1)    
+    second+=1    
+    if(second == 60):    
+        second = 0    
+        minute+=1    
+    if(minute == 60):    
+        minute = 0    
+        hours+=1    
